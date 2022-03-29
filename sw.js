a1d2e5cd650cb389d75ce9a8cb707f5e4d7e1ed5
Maxime Touroute <contact@reveality.io>
2022-03-29 21:22:43 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2ad062086be1e0d15fc3.js"
+    "url": "webpack-runtime-487aae3aed1f7b0f18f5.js"
   },
   {
     "url": "framework-ec16d78b3bcc04828a12.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bb562003ded898640986daf2d3463f1c"
+    "revision": "90573eb07d5350f3da30ba481d4026bb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4820fe49d0eb20dac8bb.js"
